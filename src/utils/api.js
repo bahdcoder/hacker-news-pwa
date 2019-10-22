c295038ca58dbdba7748237f8073de51bf9d622a
Kati Frantz <bahdcoder@gmail.com>
2019-10-23 00:37:16 +0100

    Reject failures from api class

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,10 +6,10 @@ export default new (class {
     newStories = () =>
         fetch(`${this.baseURL}/newstories.json`)
             .then(response => response.json())
-            .catch(error => error)
+            .catch(error => Promise.reject(error))
 
     getItem = itemId =>
         fetch(`${this.baseURL}/item/${itemId}.json`)
             .then(response => response.json())
-            .catch(error => error)
+            .catch(error => Promise.reject(error))
 })()
